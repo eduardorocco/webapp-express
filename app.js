0f7b756fdbedcb10bbbfb300e446735e4a1793fa
eduardorocco <eduardo_rocco@hotmail.it>
2025-01-08 16:06:38 +0100

    add 404 not found middleware and integrate it into the express server

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const notFound = require('./middlewares/notFound')
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send('Server is running')
 })
+
+app.use(notFound)
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
