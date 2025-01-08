f52548f1c8786e617bcd0561e0d20ac8fc447103
eduardorocco <eduardo_rocco@hotmail.it>
2025-01-08 16:57:40 +0100

    implement show movie functionality with reviews

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -10,8 +10,33 @@ function index(req, res) {
         }
     })}
 
-function show(req, res) {
-res.send('Show movies')
-}
+    function show(req, res) {
+        
+        const id = req.params.id
+       
+    
+        const sql = `SELECT * FROM movies WHERE id = ?`
+    
+        connection.query(sql, [id], (err, results) => {
+            if (err) return res.status(500).json({ message: err.message })
+            if (results.length === 0)
+                return res.status(404).json({
+                    error: 'Not Found',
+                    message: 'Movies not found',
+                })
+    
+            const movie = results[0]
+    
+            const sql = `SELECT * FROM reviews WHERE movie_id = ?`
+    
+            connection.query(sql, [id], (err, results) => {
+                if (err) return res.status(500).json({ message: err.message })
+    
+                movie.reviews = results
+                res.json(movie)
+            })
+        })
+    }
+    
 
 module.exports = { index, show }
