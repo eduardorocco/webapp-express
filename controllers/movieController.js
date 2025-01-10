d363d6ff744fcd839d3a27efbcd1ae07be361646
eduardorocco <eduardo_rocco@hotmail.it>
2025-01-10 17:34:33 +0100

    refactor movie image URL handling and improve SQL query for movie details

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -10,30 +10,35 @@ function index(req, res) {
     connection.query(sql, (err, movies) => {
         if (err) return res.status(500).json({ message: err.message })
         movies.forEach(movie => {
-            console.log(movie.image);
             movie.image = `http://localhost:3000/movies_cover/${movie.image}`
         })
         res.json(movies)
-        console.log(movies);
     })
 }
 
 function show(req, res) {
-
+    
     const id = req.params.id
+    
 
-
-    const sql = `SELECT * FROM movies WHERE id = ?`
+    const sql = `SELECT movies.*, AVG(vote) AS avg_vote 
+		FROM movies
+		JOIN reviews
+		ON movies.id = reviews.movie_id 
+		WHERE movies.id = ?
+		GROUP BY movies.id
+    `
 
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ message: err.message })
         if (results.length === 0)
             return res.status(404).json({
                 error: 'Not Found',
-                message: 'Movies not found',
+                message: 'Movie not found',
             })
 
         const movie = results[0]
+        movie.image = `http://localhost:3000/movies_cover/${movie.image}`
 
         const sql = `SELECT * FROM reviews WHERE movie_id = ?`
 
@@ -43,6 +48,7 @@ function show(req, res) {
             movie.reviews = results
             res.json(movie)
         })
+
     })
 }
 
